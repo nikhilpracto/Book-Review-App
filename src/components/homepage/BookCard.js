bf9204a1ccfe9994f9fb9c238cf86b6091ee710c
Nikhil Nagrale <nikhil.nagrale@practo.com>
2022-04-20 18:34:50 +0530

    homepage ready

--- src/components/homepage/BookCard.js
+++ src/components/homepage/BookCard.js
@@ -3,14 +3,19 @@ import StarRating from "../shared/starRating"
 import { Link } from 'react-router-dom'
 
 const BookCard = (props) => {
-    const averageRating = 5;
-    const URL = "/book?id=" + 1;
+    let sum = 0;
+    props.book.reviews.map((review) => { return sum = sum + review.rating })
+    const averageRating = sum / props.book.reviews.length;
+    const URL = "/book?id=" + props.book.id;
+
     return (
         <div className="bookCard">
-            <BookCover link={"https://prodimage.images-bn.com/pimages/9780393320978_p0_v2_s192x300.jpg"} />
-            <h2>Title: Hello World</h2>
-            <h2>Author: Nikhil</h2>
-            <StarRating rating={averageRating} />
+            <Link to={URL} style={{ textDecoration: "none", color: "black" }}>
+                <BookCover link={props.book.cover_image} />
+                <h2>Title: {props.book.title}</h2>
+                <h2>Author: {props.book.author}</h2>
+                <StarRating rating={averageRating} />
+            </Link>
         </div>
     )
 }
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,15 +1,47 @@
 import BookCard from "./BookCard";
 import { Header } from "./Header"
+import { connect } from 'react-redux'
+import { useEffect } from "react";
 
-const Homepage = () => {
+const mapStateToProps = (state) => {
+    return {
+        BookList: state.home.BookList
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getBooks: () => dispatch({ type: "getBook" })
+    }
+}
+
+const Homepage = ({ getBooks, BookList }) => {
+    useEffect(() => {
+        getBooks()
+    }, [])
+
+    let database;
+
+    const data = localStorage.getItem("BookDatabase");
+    if (data == null) {
+        database = BookList
+        localStorage.setItem("BookDatabase", JSON.stringify(database))
+    } else {
+        database = JSON.parse(data)
+    }
+
     return (
         <div>
             <Header />
-            <div>
-                <BookCard />
+            <div className="book--dashboard">
+                {
+                    database.map((book) => {
+                        return <BookCard key={book.id} book={book} />
+                    })
+                }
             </div>
         </div>
     )
 }
 
-export default Homepage;
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
